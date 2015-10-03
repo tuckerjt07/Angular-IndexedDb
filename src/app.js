9e7ad4c62dafe934128a27274bd442e71e7106a7
tuckerjt07 <tuckerjt07@gmail.com>
2015-10-03 16:06:52 -0500

    Added the new injection dependencies

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@
 (function () {
     'use strict';
     angular.module('IndexedDb.Utils', ['ConnectionFactory.factory', 'DatabaseObject.value', 'ObjectStore.value', 'IndecesObject.value',
-                                       'TransactionFactory.factory', 'TestController.Controller', 'Helper.factory']);
+                                       'TransactionFactory.factory', 'TestController.Controller', 'Helper.factory', 'cache.factory',
+                                      'http.factory']);
 }());
