e82ea1a2184f71bce5d576d84b34044bd66c2c54
tuckerjt07 <tuckerjt07@gmail.com>
2015-10-03 17:19:30 -0500

    Added a consistency check

--- src/cache.factory.js
+++ src/cache.factory.js
@@ -3,6 +3,23 @@
     'use strict';
     angular.module('cache.factory', [])
         .factory('CacheFactory', ['TransactionFactory', 'HttpFactory', function (TransactionFactory, HttpFactory) {
+            var consistencyCheck;
+            //Check that the cached data matches the up to date data from the server
+            consistencyCheck = function (databaseObject, objectStoreObject, indecesObject, data, apiAddress, originalCallback) {
+                var httpCallback, httpErrorCallback;
+                httpCallback = function (newData) {
+                    if (newData !== data) {
+                        TransactionFactory.insert(databaseObject, objectStoreObject, indecesObject, newData);
+                        if (originalCallback !== null) {
+                            originalCallback(newData);
+                        }
+                    }
+                };
+                httpErrorCallback = function (error) {
+                    console.log(error);
+                };
+                HttpFactory.get(apiAddress, httpCallback, httpErrorCallback);
+            };
             return {
                 //Takes the passed in database and object store and selects all data from it
                 //If the data does not exist then get it from the web service
@@ -12,6 +29,7 @@
                         if (data.length > 0) {
                             if (callback !== undefined) {
                                 callback(data);
+                                consistencyCheck(databaseObject, objectStoreObject, indecesObject, data, apiAddress, callback);
                             }
                         } else {
                             var httpCallback, httpErrorCallback;
