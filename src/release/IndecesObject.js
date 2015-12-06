e4ad7c3c556d76c49ba8516ad38afff14a7df41f
tuckerjt07 <tuckerjt07@gmail.com>
2015-12-06 10:57:49 -0600

    Added CreateObject to IndecesObject

--- src/release/IndecesObject.js
+++ src/release/IndecesObject.js
@@ -7,33 +7,37 @@
      * @module IndecesObject.value
      */
     angular.module('IndecesObject.value', [])
-    /**
-     * IndecesObject is a value that holds the indeces's information and properties
-     *
-     * @namespace IndecesObject.value
-     * @class IndecesObject
-     * @constructor
-     */
-    .value('IndecesObject', [
-        {
-            /**
-             * The name of the indece
-             *
-             * @property name
-             * @type {String}
-             * @default ""
-             */
-            name: '',
-            /**
-             * The params of the index
-             *
-             * @property params
-             * @type {Object}
-             * @default { unique: false }
-             */
-            params: {
-                unique: false
-            }
+        /**
+         * IndecesObject is a value that holds the indeces's information and properties
+         *
+         * @namespace IndecesObject.value
+         * @class IndecesObject
+         * @constructor
+         */
+        .value('IndecesObject', [
+            {
+                /**
+                 * The name of the indece
+                 *
+                 * @property name
+                 * @type {String}
+                 * @default ""
+                 */
+                name: '',
+                /**
+                 * The params of the index
+                 *
+                 * @property params
+                 * @type {Object}
+                 * @default { unique: false }
+                 */
+                params: {
+                    unique: false
+                },
+                CreateObject: function (name, params) {
+                    this.name = name;
+                    this.params = params;
+                }
             }
         ]);
 }());
